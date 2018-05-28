2506e1573f40305c4323e8aa3e5f6235632063aa
Putin <timur@wgames.com>
2018-05-28 18:42:43 -0400

    removed default client

--- lib/weddell.ex
+++ lib/weddell.ex
@@ -18,7 +18,7 @@ defmodule Weddell do
   """
   def start(_type, _args) do
     import Supervisor.Spec
-    children = [worker(Client, [])]
+    children = []
     opts = [strategy: :one_for_one, name: __MODULE__]
     Supervisor.start_link(children, opts)
   end
